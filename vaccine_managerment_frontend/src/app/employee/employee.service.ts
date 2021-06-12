1386131d435fe405a6df6992eeaa5fabd9a042bf
phamtai99 <phamtai10021999@gmail.com>
2021-06-12 22:06:46 +0700

    fix UI

--- vaccine_managerment_frontend/src/app/employee/employee.service.ts
+++ vaccine_managerment_frontend/src/app/employee/employee.service.ts
@@ -9,7 +9,7 @@ import {IEmployeeRoleDTO} from "../entity/IEmployeeRoleDTO";
 })
 export class EmployeeService {
   /*
-  * HungDH - Hien thi, sua, xoa Employee
+  * Hien thi, sua, xoa Employee
    */
   private API_EMPLOYEE = 'http://localhost:8080/api/public';
   httpOptions = {
@@ -21,13 +21,13 @@ export class EmployeeService {
     private http: HttpClient
   ) {}
 /*
-* HungDH - hien thi danh sach nhan vien
+*  hien thi danh sach nhan vien
  */
   getAllEmployee(): Observable<IEmployeeDTO[]>{
     return this.http.get<IEmployeeDTO[]>(this.API_EMPLOYEE + '/list-employee');
   }
   /*
-* HungDH - tim kiem theo id, ten, chuc vu nhan vien
+*  tim kiem theo id, ten, chuc vu nhan vien
  */
   searchEmployeeByName(name: string, idEmpSearch: string, positionSearch: string): Observable<any> {
     console.log(name + ' xyz ' + idEmpSearch + ' xyz ' + positionSearch);
@@ -35,19 +35,19 @@ export class EmployeeService {
       idEmpSearch + '&positionSearch=' + positionSearch);
   }
   /*
-* HungDH - chinh súa thong tin nhan vien
+* chinh súa thong tin nhan vien
  */
   editEmployee(employee): Observable<IEmployeeRoleDTO>{
     return this.http.put<IEmployeeRoleDTO>(this.API_EMPLOYEE + '/edit-employee/' , employee);
   }
   /*
-* HungDH - tim kien thong tin theo id
+* tim kien thong tin theo id
  */
   findById(id): Observable<IEmployeeRoleDTO> {
     return this.http.get<IEmployeeRoleDTO>(this.API_EMPLOYEE + '/find-id/'+ id, this.httpOptions);
   }
   /*
-* HungDH - xoa nhan vien
+* xoa nhan vien
  */
   deleteEmployee(id: number): Observable<any>{
     return this.http.patch<any>(this.API_EMPLOYEE + '/delete-employee/' + id, this.httpOptions);
