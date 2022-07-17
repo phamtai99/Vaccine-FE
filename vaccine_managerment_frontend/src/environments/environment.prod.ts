04032f1909a4b2dd7a3b9056edd88ca1b6b4e60c
phamtai99 <phamtai10021999@gmail.com>
2022-07-17 16:44:58 +0700

    update code

--- vaccine_managerment_frontend/src/environments/environment.prod.ts
+++ vaccine_managerment_frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:8666/',
-  // API_URL:'https://java-vaccine-systerm.herokuapp.com/'
+  // API_URL: 'http://localhost:8666/',
+  API_URL:'https://java-vaccine-systerm.herokuapp.com/'
 
 };
